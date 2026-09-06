8cb1c85c2a011951c12e3f0e88ecce88a189d29c
Luiza Prestes <luizadesouzaprestes@gmail.com>
2026-09-06 17:18:25 -0300

    alteração no index para exibir cadastros de pet do banco de dados, retirei os que estavam no html de demonstração

--- www/index.php
+++ www/index.php
@@ -1,4 +1,29 @@
-<?php require_once 'cabeçalho.php'; ?>
+<?php
+
+require_once 'conecta.php';
+
+//excluir oet
+if (isset($_GET['acao']) && $_GET['acao'] === 'excluir' && isset($_GET['id'])) {
+    $id = (int)$_GET['id'];
+    $stmt = mysqli_prepare($conn, "DELETE FROM pets WHERE id = ?");
+    if ($stmt) {
+        mysqli_stmt_bind_param($stmt, "i", $id);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_close($stmt);
+    }
+    header("Location: index.php");
+    exit;
+}
+
+$sql = "SELECT pets.*, especies.nome AS especie_nome 
+        FROM pets 
+        INNER JOIN especies ON pets.especie_id = especies.id 
+        ORDER BY pets.id DESC";
+
+$resultado = mysqli_query($conn, $sql);
+
+
+require_once 'cabeçalho.php'; ?>
 
 <div class="page-header">
     <h2>Gerenciamento de Pets</h2>
@@ -19,31 +44,29 @@
             </tr>
         </thead>
         <tbody>
+    <?php if ($resultado && mysqli_num_rows($resultado) > 0): ?>
+        <?php while ($pet = mysqli_fetch_assoc($resultado)): ?>
             <tr>
-                <td>1</td>
-                <td>Katito</td>
-                <td>2018-05-10</td>
-                <td>Gato</td>
-                <td>Macho</td>
-                <td>Vacinas em dia, castrado.</td>
+                <td><?php echo $pet['id']; ?></td>
+                <td><?php echo htmlspecialchars($pet['nome']); ?></td>
+                <td><?php echo date('d/m/Y', strtotime($pet['nascimento'])); ?></td>
+                <td><?php echo htmlspecialchars($pet['especie_nome']); ?></td>
+                <td><?php echo ucfirst($pet['genero']); ?></td>
+                <td><?php echo htmlspecialchars($pet['prontuario']); ?></td>
                 <td class="actions">
-                    <a href="form_pet.php" class="botao-editar">Editar</a>
-                    <a href="#" class="botao-deletar">Excluir</a>
+                    <a href="form_pet.php?id=<?php echo $pet['id']; ?>" class="botao-editar">Editar</a>
+                    <a href="index.php?acao=excluir&id=<?php echo $pet['id']; ?>" 
+                       class="botao-deletar" 
+                       onclick="return confirm('Tem certeza que deseja excluir este pet?');">Excluir</a>
                 </td>
             </tr>
-            <tr>
-                <td>2</td>
-                <td>Niuna</td>
-                <td>2020-11-03</td>
-                <td>Gato</td>
-                <td>Fêmea</td>
-                <td>Castrada, sem vacina de raiva.</td>
-                <td class="actions">
-                    <a href="form_pet.php" class="botao-editar">Editar</a>
-                    <a href="#" class="botao-deletar">Excluir</a>
-                </td>
-            </tr>
-        </tbody>
+        <?php endwhile; ?>
+    <?php else: ?>
+        <tr>
+            <td colspan="7" style="text-align: center; padding: 1.5rem;">Nenhum pet cadastrado até o momento.</td>
+        </tr>
+    <?php endif; ?>
+</tbody>
     </table>
 </div>
 
